079764e2468b95717c29ea0f2c357faf5f51f726
sebastian Reyes <sebreyes222gmail.com>
2023-01-04 11:04:14 +0100

    ajout objet

---
+++ public/js/index.js
@@ -0,0 +1,27 @@
+let vieille_dame = {
+    age : 89,
+    nom : {
+        prénom : 'marie',
+        Nom_De_Famile : 'juste'
+    },
+    moral : 'mal',
+    objet : 'cane',
+    parler(nom){
+        if(this.moral === 'mal'){
+            alert(`vous me derangez et elle le frappe avec sa ${this.objet}`)
+        } else {
+            alert(`Bonjour ${nom}`)
+        }
+    }
+};
+
+
+let viel_homme = {
+    nom : "jean-louis",
+    adoucir(){
+        vieille_dame.moral = 'bien'
+    }
+}
+vieille_dame.parler('monsieur jean-louis')
+viel_homme.adoucir()
+vieille_dame.parler('monsieur jean-louis')
